479a20ba8095e58646264a59449c8a8ac5c4648c
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2019-04-02 17:40:27 +0200

    mi lista

--- SQLite/SQLite/SQLite/ViewModels/TextCellViewModel.cs
+++ SQLite/SQLite/SQLite/ViewModels/TextCellViewModel.cs
@@ -18,45 +18,45 @@ namespace SQLite.ViewModels
             {
                 new Monkey
                 {
-                    Name = "Light Yagami",
-                    Location = "Kira",
-                    Details = "Es l'assasi i el protagonista principal, també qui rep la Death Note.",
-                    Image ="drawable/light.jpeg"
+                    Name = "Flash",
+                    Location = "Supervelocidad",
+                    Details = "La historia se basa en el superhéroe de DC Comics, Flash, específicamente en Barry Allen, el segundo individuo en tomar dicha identidad. Se trata además de un spin-off de Arrow, serie de televisión basada en Flecha Verde, por lo cual comparten el mismo universo de ficción",
+                    Image ="https://www.technobuffalo.com/wp-content/uploads/2016/06/The-Flash-200x200.jpg"
                 },
                new Monkey
                 {
-                    Name = "Ryuk",
-                    Location = "Shinigami",
-                    Details = "Shinigami que li pertany la death note que té en Light.",
-                    Image ="drawable/Ryuk.PNG"
+                    Name = "Superman",
+                    Location = "OP",
+                    Details = "Superman (cuyo nombre kryptoniano es Kal-El y su nombre terrestre es Clark Kent) es un personaje ficticio, un superhéroe de los cómics que aparece en las publicaciones de DC Comics",
+                    Image ="https://screenanarchy.com/assets_c/2017/05/superman-the-movie-1978-photo-01-630-thumb-860xauto-39193-thumb-200x200-66859.jpg"
                 },
                new Monkey
                 {
-                    Name = "L",
-                    Location = "Lawliet",
-                    Details = "Detectiu pricipal que intenta enxampar a Light.",
-                    Image = "drawable/L.jpg"
+                    Name = "Batman",
+                    Location = "Es rico",
+                    Details = "Batman (conocido inicialmente como The Bat-Man) es un personaje creado por los estadounidenses Bob Kane y Bill Finger,4​ y propiedad de DC Comics",
+                    Image = "https://www.technobuffalo.com/wp-content/uploads/2015/04/batman-suit-002-200x200.jpg"
                 },
                 new Monkey
                 {
-                    Name = "Misa Amane",
-                    Location = "Shinigami",
-                    Details ="Ajudant amb ulls de shinigami que ajuda en Light.",
-                    Image ="drawable/Amane.jpg"
+                    Name = "Wonder Woman",
+                    Location = "Super vuelo, súper fuerza, inmortalidad, factor de curación, super velocidad, reflejos, resistencia, brazaletes que repelen cualquier tipo de arma, habilidad de lucha altamente desarrollada y posee un lazo mágico",
+                    Details ="La Mujer Maravilla (en inglés: Wonder Woman) es una superheroína ficticia creada por William Moulton Marston para la editorial DC Comics",
+                    Image ="https://todatecnologia.com/wp-content/uploads/2016/07/Wonder-Woman-200x200.jpg"
                 },
                 new Monkey
                 {
-                    Name = "Rem",
-                    Location = "Shinigami",
-                    Details ="Shinigami que pertany a la Death note que te la Misa Amane.",
-                    Image ="drawable/Rem.PNG"
+                    Name = "Linterna Verde",
+                    Location = "Uso de la luz para crear objetos",
+                    Details ="Linterna Verde (en inglés: Green Lantern) es el alias de varios superhéroes de la ficción del Universo DC",
+                    Image ="https://images-eu.ssl-images-amazon.com/images/I/412X7C8-G+L._AC_US200_.jpg"
                 },
                 new Monkey
                 {
-                    Name = "Near",
-                    Location = "Investigador",
-                    Details ="Segon detectiu i qui descobreix en Light.",
-                    Image ="drawable/Near.PNG"
+                    Name = "Aquaman",
+                    Location = "Dominio del agua",
+                    Details ="Aquaman (cuyo verdadero nombre es Arthur Curry) es un superhéroe que aparece en los cómics estadounidenses publicados por DC Comics",
+                    Image ="https://todatecnologia.com/wp-content/uploads/2017/03/Jason-Momoa-como-Aquaman-200x200.jpg"
                 },
             };
         }
